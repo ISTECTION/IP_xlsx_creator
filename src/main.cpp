47a0948a35f6cdfce237fc40ae5fb2100af6b797
Владислав <perfectionist.one.love@outlook.com>
2024-08-20 19:42:35 +0700

    .gitignore add 'test' & set_target_properties CXX17 REQUIRED & russian locale table tabulate

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-/// : standart template library (STL)
+/// : submodule git
+#include "../vendor/tabulate/single_include/tabulate/tabulate.hpp"
 
+/// : standart template library (STL)
 #include <iostream>
 #include <locale>
 #include <filesystem>
@@ -8,13 +10,23 @@ int main (int argc, const char* argv[]) {
     ::setlocale(LC_ALL, "ru_RU.UTF8");
 
 
-    std::string _path("test");
+    std::string _path("../../test");
     std::string ext(".xlsx");
 
     for (auto &p : std::filesystem::recursive_directory_iterator(_path)) {
         if (p.path().extension() == ext)
             std::wcout << p.path().stem().wstring() << '\n';
     }
+
+
+    tabulate::Table _table;
+    _table.format().locale("ru_RU.UTF-8");
+    _table.format().multi_byte_characters(true);
+
+    _table.add_row({"ID", "КОМПАНИЯ", "ДАТА", "E-MAIL", "ЧЕКИ", "ПОДПИСЬ"});
+    _table.add_row({"tt1979376", "Toy Story 4", "Josh Cooley", "$200,000,000", "21 June 2019", "23324"});
+
+    std::cout << _table << std::endl;
 
     return 0;
 }
